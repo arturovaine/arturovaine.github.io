4cfd7d69d260a8aa67c694bc0110900b4a8ed520
arturovaine <arturo.vaine@gmail.com>
2024-12-12 23:59:23 -0300

    add artworks etc

--- js/utils.js
+++ js/utils.js
@@ -10,7 +10,11 @@ function generateContent(contentData) {
           html += `<h3>${item.title}</h3><br>`;
           html += `<p>${item.description}</p>`;
           html += item.link ? `<a href="${item.link}" target="_blank">Learn more</a><br><br>` : "";
-
+          // Apply specific style for the artworks section
+          const imgStyle = sectionKey === "artworks" 
+          ? "display: block; margin: 0 auto; height: 300px; object-fit: contain; border-radius: 10px;" 
+          : "display: block; margin: 0 auto; height: 100px; object-fit: contain;";
+          html += item.image ? `<a href="${item.link}" target="_blank"><img src="${item.image}" style="${imgStyle}" /></a><br><br>` : "";
         });
       }
       html += `</div>`;
@@ -18,3 +22,4 @@ function generateContent(contentData) {
   });
   return html;
 }
+
