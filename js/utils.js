5c5934d388a479c9f85ef2d9243436c3b1c3c943
arturovaine <arturo.vaine@gmail.com>
2024-12-15 15:53:53 -0300

    style: minor changes

--- js/utils.js
+++ js/utils.js
@@ -8,14 +8,14 @@ function generateContent(contentData) {
       
       if (section.items && section.items.length) {
         section.items.forEach((item) => {
-          html += `<h4>${item.title}</h4>`;
+          html += `<h3>${item.title}</h3>`;
           // Render description with HTML tags explicitly
           html += `<p>${item.description}</p>`;
           
           // Image styles
           const imgStyle = sectionKey === "artworks"
-            ? "display: block; margin: 0 auto; width: 300px; height: 300px; object-fit: contain; border-radius: 10px;"
-            : "display: block; margin: 0 auto; width: 100%; object-fit: contain;";
+            ? "display: block; margin: 0 auto; width: 300px; height: 300px; object-fit: contain; border-radius: 5px;"
+            : "display: block; margin: 0 auto; width: 50%; object-fit: contain; border-radius: 5px;";
 
           // Render image if available
           if (item.image) {
@@ -30,6 +30,10 @@ function generateContent(contentData) {
               <img src="${item.image_sm}" style="display: block; margin: 0 auto; height: 65px; object-fit: contain;" />
             </a><br><br>`;
           }
+
+          item.image_description
+          ? html += `<p style="font-size: small;">${item.image_description}</p>`
+          : ""
 
           // Add "Learn more" link if available
           if (item.link) {
